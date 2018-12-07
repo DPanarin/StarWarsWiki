4f4b319d412ee197363dad024d9c9e6e4041301a
DPanarin <panarin.de@gmail.com>
2018-12-07 11:56:38 +0200

    Add routing module

--- src/app/peoples/peoples.module.ts
+++ src/app/peoples/peoples.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PeoplesListComponent } from './peoples-list/peoples-list.component';
-import { PersonDetailsComponent } from './person-details/person-details.component';
+
+import { PeoplesRoutingModule } from './peoples-routing.module';
+import {PeoplesListComponent} from './peoples-list/peoples-list.component';
+import {PersonDetailsComponent} from './person-details/person-details.component';
 
 @NgModule({
   declarations: [PeoplesListComponent, PersonDetailsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PeoplesRoutingModule
   ]
 })
 export class PeoplesModule { }
